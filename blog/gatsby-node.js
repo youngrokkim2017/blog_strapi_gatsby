455d9a57600bf0709e559f5d32cae8238d674142
Patrick <patrick.yrkim@gmail.com>
2020-09-30 01:13:57 -0700

    Add article index for author

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -10,14 +10,14 @@ const path = require(`path`)
 
 const makeRequest = (graphql, request) =>
   new Promise((resolve, reject) => {
-    // Query for nodes to use in creating pages.
+    // Query for article nodes to use in creating pages.
     resolve(
       graphql(request).then(result => {
         if (result.errors) {
           reject(result.errors)
         }
 
-        return result;
+        return result
       })
     )
   })
@@ -25,7 +25,7 @@ const makeRequest = (graphql, request) =>
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+  const { createPage } = actions
 
   const getArticles = makeRequest(
     graphql,
@@ -53,6 +53,32 @@ exports.createPages = ({ actions, graphql }) => {
     })
   })
 
-  // Query for articles nodes to use in creating pages.
-  return getArticles;
+  const getAuthors = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+    `
+  ).then(result => {
+    // Create pages for each user.
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+
+  // Queries for articles and authors nodes to use in creating pages.
+  return Promise.all([getArticles, getAuthors])
 }
