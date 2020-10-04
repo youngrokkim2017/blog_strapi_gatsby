66441263a866c91eda2cc7a754a7d7902ad90d60
Patrick <patrick.yrkim@gmail.com>
2020-10-04 00:02:42 -0700

    Update category template

--- blog/src/templates/category.js
+++ blog/src/templates/category.js
@@ -3,23 +3,21 @@ import { Link, graphql } from "gatsby"
 // import Img from "gatsby-image"
 // import Layout from "../components/layout"
 
-const ArticleTemplate = ({ data }) => (
+const CategoryTemplate = ({ data }) => (
   <Layout>
-    <h1>{data.strapiArticle.title}</h1>
     <p>
-      by{" "}
-      <Link to={`/authors/User_${data.strapiArticle.author.id}`}>
-        {data.strapiArticle.author.username}
+      <Link to={`/Category_${data.strapiCategory.title}`}>
+        {data.strapiCategory.title}
       </Link>
     </p>
   </Layout>
 )
 
-export default ArticleTemplate
+export default CategoryTemplate;
 
 export const query = graphql`
-  query ArticleTemplate($id: String!) {
-    strapiArticle(id: { eq: $id }) {
+  query CategoryTemplate($id: String!) {
+    strapiCategory(id: { eq: $id }) {
       title
     }
   }
