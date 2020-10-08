2731bc2470b9d14db185bc8bfc09e20ee2b48efe
Patrick <patrick.yrkim@gmail.com>
2020-10-07 21:35:04 -0700

    Update article content type with author

--- blog/src/templates/article.js
+++ blog/src/templates/article.js
@@ -9,6 +9,84 @@ import Layout from "../components/layout"
 //     <p>
 //       by{" "}
 //       <Link to={`/authors/User_${data.strapiArticle.author.id}`}>
+//         {data.strapiArticle.author.username}
+
+//         {/* ADD THE ACTUAL WRITER'S NAME */}
+//         {/* {data.strapiArticle.submitter.name} */}
+//       </Link>
+//     </p>
+//     <Img fluid={data.strapiArticle.image.childImageSharp.fluid} />
+//     <p>{data.strapiArticle.content}</p>
+//   </Layout>
+// )
+
+// export default ArticleTemplate
+
+// export const query = graphql`
+//   query ArticleTemplate($id: String!) {
+//     strapiArticle(id: { eq: $id }) {
+//       title
+//       content
+//       image {
+//         childImageSharp {
+//           fluid(maxWidth: 500) {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+//       author {
+//         id
+//         username
+//       }
+//     }
+//   }
+// `
+
+const ArticleTemplate = ({ data }) => (
+  <Layout>
+    <h1>{data.strapiArticle.title}</h1>
+    <p>
+      by{" "}
+      <Link to={`/authors/User_${data.strapiArticle.user.id}`}>
+        {data.strapiArticle.user.username}
+
+        {/* ADD THE ACTUAL WRITER'S NAME */}
+        {/* {data.strapiArticle.submitter.name} */}
+      </Link>
+    </p>
+    <Img fluid={data.strapiArticle.image.childImageSharp.fluid} />
+    <p>{data.strapiArticle.content}</p>
+  </Layout>
+)
+
+export default ArticleTemplate
+
+export const query = graphql`
+  query ArticleTemplate($id: String!) {
+    strapiArticle(id: { eq: $id }) {
+      title
+      content
+      image {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+
+// const ArticleTemplate = ({ data }) => (
+//   <Layout>
+//     <h1>{data.strapiArticle.title}</h1>
+//     <p>
+//       by{" "}
+//       <Link to={`/authors/User_${data.strapiArticle.author.id}`}>
 //         {data.strapiArticle.author.name}
 //       </Link>
 //     </p>
@@ -38,42 +116,3 @@ import Layout from "../components/layout"
 //     }
 //   }
 // `
-
-const ArticleTemplate = ({ data }) => (
-  <Layout>
-    <h1>{data.strapiArticle.title}</h1>
-    <p>
-      by{" "}
-      <Link to={`/authors/User_${data.strapiArticle.author.id}`}>
-        {data.strapiArticle.author.username}
-
-        {/* ADD THE ACTUAL WRITER'S NAME */}
-        {/* {data.strapiArticle.submitter.name} */}
-      </Link>
-    </p>
-    <Img fluid={data.strapiArticle.image.childImageSharp.fluid} />
-    <p>{data.strapiArticle.content}</p>
-  </Layout>
-)
-
-export default ArticleTemplate
-
-export const query = graphql`
-  query ArticleTemplate($id: String!) {
-    strapiArticle(id: { eq: $id }) {
-      title
-      content
-      image {
-        childImageSharp {
-          fluid(maxWidth: 500) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      author {
-        id
-        username
-      }
-    }
-  }
-`
