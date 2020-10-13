7de9ada7d7511e6d2a9c3782415c4502bb68d09c
Patrick <patrick.yrkim@gmail.com>
2020-10-12 23:35:11 -0700

    Clean up commented code

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -6,10 +6,6 @@ import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p> */}
-
     <ul>
       {data.allStrapiArticle.edges.map(document => (
         <li key={document.node.id}>
