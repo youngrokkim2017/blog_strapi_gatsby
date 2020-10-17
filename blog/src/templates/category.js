bbb4854a46e9b66b1edaad03646459c25aa59883
Patrick <patrick.yrkim@gmail.com>
2020-10-16 23:37:28 -0700

    Fix strapi errors

--- blog/src/templates/category.js
+++ blog/src/templates/category.js
@@ -1,30 +1,30 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-// import Img from "gatsby-image"
-// import Layout from "../components/layout"
+// import React from "react"
+// import { Link, graphql } from "gatsby"
+// // import Img from "gatsby-image"
+// // import Layout from "../components/layout"
 
-const CategoryTemplate = ({ data }) => (
-  <Layout>
-    <p>
-      <Link to={`/Category_${data.strapiCategory.title}`}>
-        {data.strapiCategory.title}
-      </Link>
-    </p>
-  </Layout>
-)
+// const CategoryTemplate = ({ data }) => (
+//   <Layout>
+//     <p>
+//       <Link to={`/Category_${data.strapiCategory.title}`}>
+//         {data.strapiCategory.title}
+//       </Link>
+//     </p>
+//   </Layout>
+// )
 
-export default CategoryTemplate;
+// export default CategoryTemplate;
 
-export const query = graphql`
-  query CategoryTemplate($id: String!) {
-    strapiCategory(id: { eq: $id }) {
-      title
-      article {
-          id
-          title
-          author
-          content
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query CategoryTemplate($id: String!) {
+//     strapiCategory(id: { eq: $id }) {
+//       title
+//       article {
+//           id
+//           title
+//           author
+//           content
+//       }
+//     }
+//   }
+// `
