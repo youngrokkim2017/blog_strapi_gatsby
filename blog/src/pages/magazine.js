953ca10dc13807c157087d16e2cb47415a82708b
Patrick <patrick.yrkim@gmail.com>
2020-10-28 23:43:09 -0700

    Update styling

--- blog/src/pages/magazine.js
+++ blog/src/pages/magazine.js
@@ -7,9 +7,10 @@ import SEO from "../components/seo"
 const MagazinePage = () => (
   <Layout>
     <SEO title="Magazine page" />
+    {/* <Link to="/">Go back to the homepage</Link> */}
+    <Link to="/" style={{textDecoration: `none`}}>Go back to the homepage</Link>
     <h1>Hi from the Magazine page</h1>
     <p>Welcome to magazine page</p>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
