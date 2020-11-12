b330736ce2bf7b27cb0d6c5c584ce5a30ea3e6eb
Patrick <patrick.yrkim@gmail.com>
2020-11-12 02:04:20 -0800

    Update search index

--- blog/src/pages/search-index.js
+++ blog/src/pages/search-index.js
@@ -40,6 +40,7 @@ const SearchPage = ({ data }) => {
     //     ],
     //     includeScore: true,
     // };
+    // //
     
     // // PRE-GENERATED INDEX SEARCH
     // const optionsIndexed = {
@@ -53,8 +54,9 @@ const SearchPage = ({ data }) => {
     // const newIndex = Fuse.createIndex(optionsIndexed.keys, unsortedData);
     // const fuse = new Fuse(unsortedData, optionsIndexed, newIndex);
     // console.log(fuse)
+    // //
 
-    // FUSE JS 
+    // // FUSE JS 
     // const fuse = new Fuse(unsortedData, {
     //     keys: [
     //         'node.title',
@@ -65,7 +67,7 @@ const SearchPage = ({ data }) => {
     // });
 
     // const fuse = new Fuse(unsortedData, options);
-    // 
+    // //
 
     // console.log('fuse', fuse);
     // const results = fuse.search('science');
@@ -73,7 +75,8 @@ const SearchPage = ({ data }) => {
     // console.log(results);
 
     // const searchResults = results.map(result => result.item)
-    const searchResults = query ? results.map(result => result.item) : unsortedData.reverse();
+    // const searchResults = query ? results.map(result => result.item) : unsortedData.reverse();
+    const searchResults = query ? results.map(result => result.item) : unsortedData.reverse().slice(0, 5);
     // console.log(searchResults);
     // console.log(query);
 
@@ -95,31 +98,8 @@ const SearchPage = ({ data }) => {
             </form>
         </div>
 
-        {/* <ul>
-            {data.allStrapiArticle.edges.reverse().map(document => (
-                <li key={document.node.id}>
-                    <h2>
-                        <Link to={`/blog/${document.node.id}`} style={{ textDecoration: `none` }}>
-                            {document.node.title}
-                        </Link>
-                    </h2>
-                    <h4>By{" "}{document.node.author}</h4>
-                {
-                    document.node.image
-                    ?
-                    <Img fixed={document.node.image.childImageSharp.fixed} />
-                    :
-                    ""
-                }
-                <Reactmarkdown
-                    source={document.node.content}
-                    transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
-                />
-                </li>
-            ))}
-        </ul> */}
-
         <ul>
+            {/* data.allStrapiArticle.edges.reverse().map(document => ( */}
             {/* {searchResults.reverse().map(document => ( */}
             {searchResults.map(document => (
                 <li key={document.node.id}>
@@ -176,100 +156,3 @@ export const searchQuery = graphql`
     }
   }
 `
-
-// import React, { useReact } from "react"
-// import { Link, graphql } from "gatsby"
-
-// const BlogIndex = props => {
-//   const { data } = props
-//   const allPosts = data.allMarkdownRemark.edges
-//   const emptyQuery = ""
-//   const [state, setState] = useState({
-//     filteredData: [],
-//     query: emptyQuery,
-//   })
-//   const handleInputChange = event => {
-//     console.log(event.target.value)
-//     const query = event.target.value
-//     const { data } = props
-//     const posts = data.allMarkdownRemark.edges || []
-//     const filteredData = posts.filter(post => {
-//       const { description, title, tags } = post.node.frontmatter
-//       return (
-//         description.toLowerCase().includes(query.toLowerCase()) ||
-//         title.toLowerCase().includes(query.toLowerCase()) ||
-//         (tags &&
-//           tags
-//             .join("")
-//             .toLowerCase()
-//             .includes(query.toLowerCase()))
-//       )
-//     })
-//     setState({
-//       query,
-//       filteredData,
-//     })
-//   }
-//   const { filteredData, query } = state
-//   const hasSearchResults = filteredData && query !== emptyQuery
-//   const posts = hasSearchResults ? filteredData : allPosts
-//   return (
-//     <>
-//       <h1 style={{ textAlign: `center` }}>Writing</h1>
-//       <div className="searchBox">
-//         <input
-//           className="searchInput"
-//           type="text"
-//           aria-label="Search"
-//           placeholder="Type to filter posts..."
-//           onChange={handleInputChange}
-//         />
-//       </div>
-//       {posts.map(({ node }) => {
-//         const { excerpt } = node
-//         const { slug } = node.fields
-//         const { tags, title, date, description } = node.frontmatter
-//         return (
-//           <article key={slug}>
-//             <header>
-//               <h2>
-//                 <Link to={slug}>{title}</Link>
-//               </h2>
-//               <p>{date}</p>
-//             </header>
-//             <section>
-//               <p
-//                 dangerouslySetInnerHTML={{
-//                   __html: description || excerpt,
-//                 }}
-//               />
-//             </section>
-//             <hr />
-//           </article>
-//         )
-//       })}
-//     </>
-//   )
-// }
-// export default BlogIndex
-// export const pageQuery = graphql`
-//   query {
-//     allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
-//       edges {
-//         node {
-//           excerpt(pruneLength: 200)
-//           id
-//           frontmatter {
-//             title
-//             description
-//             date(formatString: "MMMM DD, YYYY")
-//             tags
-//           }
-//           fields {
-//             slug
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
