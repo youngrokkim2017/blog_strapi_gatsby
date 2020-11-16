83cc6a3a63b0ae462d73ac2822736174a5778adb
Patrick <patrick.yrkim@gmail.com>
2020-11-16 03:20:18 -0800

    Add comment system

--- cms/api/article/controllers/article.js
+++ cms/api/article/controllers/article.js
@@ -1,38 +1,55 @@
-// 'use strict';
+// // 'use strict';
 
-// /**
-//  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
-//  * to customize this controller
-//  */
+// // /**
+// //  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+// //  * to customize this controller
+// //  */
 
-// module.exports = {
-//     // update article record
-//     // https://youtu.be/ITk-pYtOCnQ?t=297
+// // module.exports = {
+// //     // update article record
+// //     // https://youtu.be/ITk-pYtOCnQ?t=297
 
-//     // async update(ctx) {}
+// //     // async update(ctx) {}
 
     
+// // };
+
+// const { sanitizeEntity } = require('strapi-utils');
+
+// module.exports = {
+//   async find(ctx) {
+//     let entities;
+
+//     // const filters = ctx.query;
+//     // filters.published = true;
+
+//     if (ctx.query._q) {
+//       entities = await strapi.services.article.search(ctx.query);
+//     //   entities = await strapi.services.article.search(filters);
+//     } else {
+//       entities = await strapi.services.article.find(ctx.query);
+//     //   entities = await strapi.services.article.find(filters);
+//     }
+
+//     // entities = entities.sort((a, b) => b.id - a.id);
+
+//     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.article }));
+//   },
 // };
 
-const { sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
-  async find(ctx) {
-    let entities;
-
-    // const filters = ctx.query;
-    // filters.published = true;
-
-    if (ctx.query._q) {
-      entities = await strapi.services.article.search(ctx.query);
-    //   entities = await strapi.services.article.search(filters);
+  async comment(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.comment.create(data, { files });
     } else {
-      entities = await strapi.services.article.find(ctx.query);
-    //   entities = await strapi.services.article.find(filters);
+        ctx.request.body.article = ctx.params.id;
+
+      entity = await strapi.services.comment.create(ctx.request.body);
     }
-
-    // entities = entities.sort((a, b) => b.id - a.id);
-
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.article }));
+    return sanitizeEntity(entity, { model: strapi.models.comment });
   },
 };
