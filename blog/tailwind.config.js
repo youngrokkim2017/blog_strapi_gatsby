4ee31c8eeea50d99752649d722c4478c7548707d
Kamran Ahmed <k.ahmed@berkeley.edu>
2020-11-21 01:42:02 -0800

    Update regex to catch inline images, add subtle article styles

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -5,9 +5,37 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    typography: {
+      default: {
+        css: {
+          color: 'rgb(51,51,51)',
+          // blockquote: {
+          //   fontWeight: '500',
+          //   fontSize: 'rem(10)',
+          //   fontStyle: null,
+          //   borderLeftWidth: null,
+          //   borderLeftColor: null,
+          //   quotes: null,
+          // },
+          // 'blockquote p:first-of-type::before': {
+          //   content: null,
+          // },
+          // 'blockquote p:last-of-type::after': {
+          //   content: null,
+          // },
+        },
+      },
+      // 'lg': {
+      //   css: {
+      //     blockquote: {
+      //       padding: '0',
+      //     },
+      //   },
+      // },
+    },
   },
   variants: {},
+  important: true,
   plugins: [
     require('@tailwindcss/typography'),
   ],
