99bc482f945aa65e5eaf5de6227e0155d5eeb5e9
Patrick <patrick.yrkim@gmail.com>
2020-12-01 22:22:41 -0800

    Remove unused directories

--- blog/src/pages/search-index.js
+++ blog/src/pages/search-index.js
@@ -1,187 +1,187 @@
-import React, { useState } from "react"
-import { Link, graphql } from "gatsby"
-import Img from 'gatsby-image';
-import Layout from "../components/layout"
-import Reactmarkdown from "react-markdown"
-import SEO from "../components/seo"
-import Fuse from "fuse.js"  // fuzzy search
+// import React, { useState } from "react"
+// import { Link, graphql } from "gatsby"
+// import Img from 'gatsby-image';
+// import Layout from "../components/layout"
+// import Reactmarkdown from "react-markdown"
+// import SEO from "../components/seo"
+// import Fuse from "fuse.js"  // fuzzy search
 
-const SearchPage = ({ data }) => {
-    const [query, setQuery] = useState('');
-    // const [input, setInput] = useState('');
+// const SearchPage = ({ data }) => {
+//     const [query, setQuery] = useState('');
+//     // const [input, setInput] = useState('');
 
-    // const unsortedData = data.allStrapiArticle.edges.reverse();
+//     // const unsortedData = data.allStrapiArticle.edges.reverse();
 
-    const unsortedData = data.allStrapiArticle.edges;
-    const sortedData = unsortedData.sort((a, b) => b.node.id.split('_')[1] - a.node.id.split('_')[1]);
+//     const unsortedData = data.allStrapiArticle.edges;
+//     const sortedData = unsortedData.sort((a, b) => b.node.id.split('_')[1] - a.node.id.split('_')[1]);
 
-    const options = {
-        keys: [
-            'node.title',
-            // 'node.author',
-            // 'node.content',
-        ],
-        includeScore: true,
-    };
+//     const options = {
+//         keys: [
+//             'node.title',
+//             // 'node.author',
+//             // 'node.content',
+//         ],
+//         includeScore: true,
+//     };
 
-    const fuse = new Fuse(unsortedData, options);
+//     const fuse = new Fuse(unsortedData, options);
 
 
-    // // WEIGHTED SEARCH
-    // const optionsWeighted = {
-    //     keys: [
-    //         {
-    //             name: 'node.title',
-    //             weight: 0.6,
-    //         },
-    //         {
-    //             name: 'node.author',
-    //             weight: 0.1,
-    //         },
-    //         {
-    //             name: 'node.content',
-    //             weight: 0.3,
-    //         },
-    //     ],
-    //     includeScore: true,
-    // };
-    // //
+//     // // WEIGHTED SEARCH
+//     // const optionsWeighted = {
+//     //     keys: [
+//     //         {
+//     //             name: 'node.title',
+//     //             weight: 0.6,
+//     //         },
+//     //         {
+//     //             name: 'node.author',
+//     //             weight: 0.1,
+//     //         },
+//     //         {
+//     //             name: 'node.content',
+//     //             weight: 0.3,
+//     //         },
+//     //     ],
+//     //     includeScore: true,
+//     // };
+//     // //
     
-    // // PRE-GENERATED INDEX SEARCH
-    // const optionsIndexed = {
-    //     keys: [
-    //         'node.title',
-    //         // 'node.author',
-    //         // 'node.content',
-    //     ],
-    //     includeScore: true,
-    // }
-    // const newIndex = Fuse.createIndex(optionsIndexed.keys, unsortedData);
-    // const fuse = new Fuse(unsortedData, optionsIndexed, newIndex);
-    // console.log(fuse)
-    // //
+//     // // PRE-GENERATED INDEX SEARCH
+//     // const optionsIndexed = {
+//     //     keys: [
+//     //         'node.title',
+//     //         // 'node.author',
+//     //         // 'node.content',
+//     //     ],
+//     //     includeScore: true,
+//     // }
+//     // const newIndex = Fuse.createIndex(optionsIndexed.keys, unsortedData);
+//     // const fuse = new Fuse(unsortedData, optionsIndexed, newIndex);
+//     // console.log(fuse)
+//     // //
 
-    // // FUSE JS 
-    // const fuse = new Fuse(unsortedData, {
-    //     keys: [
-    //         'node.title',
-    //         // 'node.author',
-    //         // 'node.content',
-    //     ],
-    //     includeScore: true,
-    // });
+//     // // FUSE JS 
+//     // const fuse = new Fuse(unsortedData, {
+//     //     keys: [
+//     //         'node.title',
+//     //         // 'node.author',
+//     //         // 'node.content',
+//     //     ],
+//     //     includeScore: true,
+//     // });
 
-    // const fuse = new Fuse(unsortedData, options);
-    // //
+//     // const fuse = new Fuse(unsortedData, options);
+//     // //
 
-    // console.log('fuse', fuse);
-    // const results = fuse.search('science');
-    const results = fuse.search(query);
-    // console.log(results);
+//     // console.log('fuse', fuse);
+//     // const results = fuse.search('science');
+//     const results = fuse.search(query);
+//     // console.log(results);
 
-    // const searchResults = results.map(result => result.item)
-    // const searchResults = query ? results.map(result => result.item) : unsortedData.reverse();
-    // const searchResults = query.length > 3 ? results.map(result => result.item) : unsortedData.slice(0, 5);
-    const searchResults = query.length > 3 ? results.map(result => result.item) : sortedData.slice(0, 5);
-    // const searchResults = input ? results.map(result => result.item) : unsortedData.slice(0, 5);
-    // console.log(searchResults);
-    // console.log(query);
+//     // const searchResults = results.map(result => result.item)
+//     // const searchResults = query ? results.map(result => result.item) : unsortedData.reverse();
+//     // const searchResults = query.length > 3 ? results.map(result => result.item) : unsortedData.slice(0, 5);
+//     const searchResults = query.length > 3 ? results.map(result => result.item) : sortedData.slice(0, 5);
+//     // const searchResults = input ? results.map(result => result.item) : unsortedData.slice(0, 5);
+//     // console.log(searchResults);
+//     // console.log(query);
 
-    // const sortedResults = results.sort((a, b) => a.score - b.score);
-    // const sortedResults = searchResults.sort((a, b) => a.score - b.score);
+//     // const sortedResults = results.sort((a, b) => a.score - b.score);
+//     // const sortedResults = searchResults.sort((a, b) => a.score - b.score);
 
-    function handleOnSearch({ currentTarget = {} }) {
-        const { value } = currentTarget;
-        setQuery(value);
-    }
+//     function handleOnSearch({ currentTarget = {} }) {
+//         const { value } = currentTarget;
+//         setQuery(value);
+//     }
 
-    // function handleOnSubmit(e) {
-    //     setInput(query)
-    //     e.preventDefault();
-    // }
+//     // function handleOnSubmit(e) {
+//     //     setInput(query)
+//     //     e.preventDefault();
+//     // }
 
-    return (
-        <Layout>
-        <SEO title="Search index page" />
+//     return (
+//         <Layout>
+//         <SEO title="Search index page" />
 
-        <div>
-            <form>
-                <input 
-                    type="text" 
-                    placeholder="Search" 
-                    value={query} 
-                    onChange={handleOnSearch} 
-                />
-            </form>
-            {/* <form onSubmit={handleOnSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Search" 
-                    value={query} 
-                    onChange={handleOnSearch} 
-                />
-                <input type="submit" value="submit" />
-            </form> */}
-        </div>
+//         <div>
+//             <form>
+//                 <input 
+//                     type="text" 
+//                     placeholder="Search" 
+//                     value={query} 
+//                     onChange={handleOnSearch} 
+//                 />
+//             </form>
+//             {/* <form onSubmit={handleOnSubmit}>
+//                 <input 
+//                     type="text" 
+//                     placeholder="Search" 
+//                     value={query} 
+//                     onChange={handleOnSearch} 
+//                 />
+//                 <input type="submit" value="submit" />
+//             </form> */}
+//         </div>
 
-        <ul>
-            {searchResults.map(document => (
-                <li key={document.node.id}>
-                    <h2>
-                        <Link to={`/blog/${document.node.id}`} style={{ textDecoration: `none` }}>
-                            {document.node.title}
-                        </Link>
-                    </h2>
-                    <h4>By{" "}{document.node.author}</h4>
-                {
-                    document.node.image
-                    ?
-                    <Img fixed={document.node.image.childImageSharp.fixed} />
-                    :
-                    ""
-                }
-                <Reactmarkdown
-                    // source={document.node.content}
-                    source={`${document.node.content.slice(0,500)}...`}
-                    transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
-                />
-                </li>
-            ))}
-        </ul>
-        </Layout>
-    )
-}
+//         <ul>
+//             {searchResults.map(document => (
+//                 <li key={document.node.id}>
+//                     <h2>
+//                         <Link to={`/blog/${document.node.id}`} style={{ textDecoration: `none` }}>
+//                             {document.node.title}
+//                         </Link>
+//                     </h2>
+//                     <h4>By{" "}{document.node.author}</h4>
+//                 {
+//                     document.node.image
+//                     ?
+//                     <Img fixed={document.node.image.childImageSharp.fixed} />
+//                     :
+//                     ""
+//                 }
+//                 <Reactmarkdown
+//                     // source={document.node.content}
+//                     source={`${document.node.content.slice(0,500)}...`}
+//                     transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+//                 />
+//                 </li>
+//             ))}
+//         </ul>
+//         </Layout>
+//     )
+// }
 
-export default SearchPage;
+// export default SearchPage;
 
-// gql query
-export const searchQuery = graphql`
-  query SearchQuery {
-    allStrapiArticle(
-      limit: 5
-      sort: { order: DESC, fields: published_at }
-    ) {
-      edges {
-        node {
-          id
-          image {
-            childImageSharp {
-              fixed(width: 200, height: 125) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-          title
-          author
-          content
-          category {
-            id
-            title
-          }
-          published_at
-          updated_at
-        }
-      }
-    }
-  }
-`
+// // gql query
+// export const searchQuery = graphql`
+//   query SearchQuery {
+//     allStrapiArticle(
+//       limit: 5
+//       sort: { order: DESC, fields: published_at }
+//     ) {
+//       edges {
+//         node {
+//           id
+//           image {
+//             childImageSharp {
+//               fixed(width: 200, height: 125) {
+//                 ...GatsbyImageSharpFixed
+//               }
+//             }
+//           }
+//           title
+//           author
+//           content
+//           category {
+//             id
+//             title
+//           }
+//           published_at
+//           updated_at
+//         }
+//       }
+//     }
+//   }
+// `
