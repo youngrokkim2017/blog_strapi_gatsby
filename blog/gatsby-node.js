dfc5123109e2f1f99e46f88388b531cc27066007
Patrick <patrick.yrkim@gmail.com>
2020-12-16 01:56:58 -0800

    Update createPages with async and await

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -49,32 +49,33 @@ exports.createPages = ({ actions, graphql }) => {
   //   })
   // }
 
-  const getAuthors = makeRequest(
-    graphql,
-    `
-    {
-      allStrapiUser {
-        edges {
-          node {
-            id
-            name
-          }
-        }
-      }
-    }
-    `
-  ).then(result => {
-    // Create pages for each user.
-    result.data.allStrapiUser.edges.forEach(({ node }) => {
-      createPage({
-        path: `/authors/${node.id}`,
-        component: path.resolve(`src/templates/author.js`),
-        context: {
-          id: node.id,
-        },
-      })
-    })
-  })
+  // // AUTHOR CONTENT TYPE
+  // const getAuthors = makeRequest(
+  //   graphql,
+  //   `
+  //   {
+  //     allStrapiUser {
+  //       edges {
+  //         node {
+  //           id
+  //           name
+  //         }
+  //       }
+  //     }
+  //   }
+  //   `
+  // ).then(result => {
+  //   // Create pages for each user.
+  //   result.data.allStrapiUser.edges.forEach(({ node }) => {
+  //     createPage({
+  //       path: `/authors/${node.id}`,
+  //       component: path.resolve(`src/templates/author.js`),
+  //       context: {
+  //         id: node.id,
+  //       },
+  //     })
+  //   })
+  // })
 
   // BLOG CONTENT TYPES
   const getArticles = makeRequest(
@@ -239,7 +240,7 @@ exports.createPages = ({ actions, graphql }) => {
 
   // Queries for articles and authors nodes to use in creating pages.
   return Promise.all([
-    getAuthors,
+    // getAuthors,
     getArticles, 
     getCategories,
     getIssues,
@@ -248,3 +249,153 @@ exports.createPages = ({ actions, graphql }) => {
     getSubscribe,
   ])
 }
+
+
+// // AWAIT / ASYNC
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage, createRedirect } = actions
+
+//   const result = await graphql(`
+//     query {
+//       allStrapiUser {
+//         edges {
+//           node {
+//             id
+//             name
+//           }
+//         }
+//       }
+//       allStrapiArticle (sort: { fields: [created_at], order: DESC }) {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//       allStrapiCategory {
+//         edges {
+//           node {
+//             id
+//             title
+//           }
+//         }
+//       }
+//       allStrapiIssue {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//       allStrapiTag {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//       allStrapiAbout {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//       allStrapiSubscribe {
+//         edges {
+//           node {
+//             id
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   // const posts = result.data.allStrapiArticle.nodes
+//   // const postsPerPage = 6
+//   // const numPages = Math.ceil(posts.length / postsPerPage)
+//   // Array.from({ length: numPages }).forEach((_, i) => {
+//   //   createPage({
+//   //     path: i === 0 ? `/archive` : `/archive/page/${i + 1}`,
+//   //     component: path.resolve('./src/templates/blog-page.js'),
+//   //     context: {
+//   //       limit: postsPerPage,
+//   //       skip: i * postsPerPage,
+//   //       numPages,
+//   //       currentPage: i + 1,
+//   //     },
+//   //   })
+//   // })
+
+//   // result.data.allStrapiUser.edges.forEach(({ node }) => {
+//   //   createPage({
+//   //     path: `/authors/${node.id}`,
+//   //     component: path.resolve(`src/templates/author.js`),
+//   //     context: {
+//   //       id: node.id,
+//   //     },
+//   //   })
+//   // })
+
+//   result.data.allStrapiArticle.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/blog/${node.id}`,
+//       component: path.resolve(`src/templates/article.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+
+//   result.data.allStrapiCategory.edges.forEach(({ node }) => {
+//     createPage({
+//       // path: `/categories/${node.id}`,
+//       path: `/categories/${node.title.split(" ").join("-")}`,
+//       component: path.resolve(`src/templates/category.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+
+//   result.data.allStrapiIssue.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/magazine/${node.id}`,
+//       component: path.resolve(`src/templates/issue.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+
+//   result.data.allStrapiTag.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/tags/${node.id}`,
+//       component: path.resolve(`src/templates/tag.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+
+//   result.data.allStrapiAbout.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/about/`,
+//       component: path.resolve(`src/templates/about.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+
+//   result.data.allStrapiSubscribe.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/subscribe/`,
+//       component: path.resolve(`src/templates/subscribe.js`),
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   }) 
+// }
