7dd5eff167ebe7fe952d499ec267174a61a9ff7f
Kamran Ahmed <k.ahmed@berkeley.edu>
2020-12-26 13:59:51 -0800

    Add components for search bar toggle:

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -19,6 +19,13 @@ const Header = () => {
     )
   }
 
+  function toggleSearchBar() {
+    // x = document.getElementById("search-input");
+    // x.classList.remove("hidden");
+    // x.classList.add("block");
+    // console.log("test");
+  }
+
   return (
     <nav className="text-black mb-12 container mx-auto sans-serif">
       <div className="border-b py-4" style={{ borderBottomColor: '#ECECF3' }}>
@@ -33,25 +40,33 @@ const Header = () => {
               <img src={logo} alt="Logo" className="sm:h-10 mx-auto" />
             </Link>
           </div>
-          <div className="relative w-1/4 flex justify-end">
-            <form onSubmit={handleNavigate}>
-              {/* <input
-                type="text"
-                placeholder="Search"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-              /> */}
-              {/* <Link to={'/blog'} state={{ searchQuery: query }}>SEARCH</Link> */}
-              <button type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-black">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-                {/* <span>Quick search
-                      <span class="hidden sm:inline"> for anything
-                      </span>
-                </span> */}
-              </button>
-            </form>
+          <div className="relative w-1/4 flex justify-end items-center">
+            <div className="block" id="search-input">
+              <form onSubmit={handleNavigate} className="border-black text-gray-600 flex items-center py-1 px-2 pr-1 pl-0 border-b focus-within:border-blue-600">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  className="bg-transparent border-none w-full text-black placeholder-gray-600 leading-tight focus:outline-none mr-2"
+                />
+                <button type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-black">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                  </svg>
+                </button>
+              </form>
+            </div>
+            <button type="submit" className="inline-block pl-2 py-1 leading-none text-black flex-shrink-0 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+            <button onClick={toggleSearchBar} className="inline-block py-1 leading-none text-black flex-shrink-0 cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-black">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
@@ -71,47 +86,27 @@ const Header = () => {
               }
             `}
             render={data => (
-              <div className="text-sm space-x-4 mx-auto">
+              <div className="text-base space-x-4 mx-auto">
                 {data.allStrapiCategory.edges.map((document, idx) => (
 
-                  <Link 
+                  <Link
                     // to={`/categories/${document.node.id}`} 
-                    to={`/categories/${document.node.title.split(" ").map((category) => category.toLowerCase()).join("-")}`} 
+                    to={`/categories/${document.node.title.split(" ").map((category) => category.toLowerCase()).join("-")}`}
                     // to={`/categories/${document.node.title.split(" ").join("-")}/1`} 
-                    key={idx} 
+                    key={idx}
                     className="block mt-4 lg:inline-block lg:mt-0">
                     {document.node.title}
                   </Link>
                 ))}
 
-                <Link 
-                  to="/archive/1" 
+                <Link
+                  to="/archive/1"
                   className="block mt-4 lg:inline-block lg:mt-0">
                   Archive
                 </Link>
               </div>
             )}
           />
-          <div>
-            <form onSubmit={handleNavigate}>
-              <input
-                type="text"
-                placeholder="Search"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-              />
-              {/* <Link to={'/blog'} state={{ searchQuery: query }}>SEARCH</Link> */}
-              <button type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-black">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-                {/* <span>Quick search
-                      <span class="hidden sm:inline"> for anything
-                      </span>
-                </span> */}
-              </button>
-            </form>
-          </div>
         </div>
       </div>
 
