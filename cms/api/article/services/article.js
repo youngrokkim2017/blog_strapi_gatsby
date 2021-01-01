65af51fdc26feb746ff6f79b70ed350403713995
Patrick <patrick.yrkim@gmail.com>
2021-01-01 00:38:16 -0800

    Add strapi services to get relation data

--- cms/api/article/services/article.js
+++ cms/api/article/services/article.js
@@ -6,3 +6,13 @@
  */
 
 module.exports = {};
+
+// module.exports = {
+//   async find(params, populate) {
+//     let articles = await strapi.query("article").find(params, populate);
+//     // let author = await strapi.query("author").findOne({ id: authorId }, populate);
+//     let author = await strapi.query("authors").findOne({ id: authorId }, populate);
+//     articles.author = author
+//     return articles;
+//   },
+// };
