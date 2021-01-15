555a01d440a96471e8507769398b445d0cb16b59
Patrick <patrick.yrkim@gmail.com>
2021-01-15 00:08:01 -0800

    Add splash page data

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,6 +4,24 @@ import { graphql } from "gatsby"
 // import { Link, graphql } from "gatsby"
 
 const IndexPage = ({ data }) => {
+  const sortedByDate = this.props.data.allStrapiArticle.edges.sort((a, b) => {
+    let aDate = parseInt(a.node.published_at.split("T")[0].split("-").join(""))
+    let bDate = parseInt(b.node.published_at.split("T")[0].split("-").join(""))
+    return (bDate - aDate)
+  });
+
+  const recentArticles = sortedByDate.slice(0, 3);
+
+  const labscopeArticles = sortedByDate.filter(document => (
+    document.node.categories.map(cat => cat.title === 'Labscope')
+    // document.node.categories[0].title === 'Labscope'
+  )).slice(0, 5);
+
+  const noteworthyArticles = sortedByDate.filter(document => (
+    document.node.categories.map(cat => cat.title === 'Noteworthy News')
+    // document.node.categories[0].title === 'Noteworthy News'
+  )).slice(0, 5);
+
   return (
     <Layout>
       <div className="bg-red-300">HOME PAGE</div>
@@ -17,7 +35,6 @@ export default IndexPage;
 export const splashQuery = graphql`
   query SplashQuery {
     allStrapiArticle(
-      limit: 5
       sort: { order: DESC, fields: published_at }
     ) {
       edges {
